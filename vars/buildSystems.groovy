a5c781ed0c0452ec4e0c74f0f69f6a2fec9c6759
aravinddsrc <aravind.a@dsrc.co.in>
2021-05-17 13:28:19 +0530

    Update buildSystems.groovy

--- vars/buildSystems.groovy
+++ vars/buildSystems.groovy
@@ -8,13 +8,13 @@ static def reportQualityGate(script, organisation, repository, status, context, 
             target_url: script.env.RUN_DISPLAY_URL
     ]
 
-    def jsonRequest = JsonOutput.toJson(request)
+    def jsonRequestdata = JsonOutput.toJson(request)
     script.httpRequest(
             acceptType: 'APPLICATION_JSON',
             authentication: 'feb40616d4d730b6c89a9f74aafe93a3e05230fb',
             contentType: 'APPLICATION_JSON',
             httpMode: 'POST',
-            requestBody: jsonRequest,
+            requestBody: jsonRequestdata,
             responseHandle: 'NONE',
             url: "http://gitrepsrv:3000/api/v1/repos/$organisation/$repository/statuses/$currentSha")
 }
