33a2a649dc910d55e4dd34dc8b7c7c0ee82d5c8b
aravinddsrc <aravind.a@dsrc.co.in>
2021-05-17 23:33:02 +0530

    Update buildSystems.groovy

--- vars/buildSystems.groovy
+++ vars/buildSystems.groovy
@@ -23,11 +23,10 @@ def reportQualityGate(script, Organisation, repository, status, context, descrip
     println  "${jsonRequestdata}"
     println  "http://gitrepsrv:3000/api/v1/repos/${Organisation}/${repository}/statuses/${currentSha}"
     
-    bat("curl --location --request POST 'http://gitrepsrv:3000/api/v1/repos/aravind.a/GitSync/statuses/444b84683bd56fb3f9755c1d344802c653a9d91f' \
-    --header 'Authorization: Bearer feb40616d4d730b6c89a9f74aafe93a3e05230fb' \
-    --header 'Content-Type: application/json' \
-    --header 'Cookie: lang=en-US; i_like_gitea=065687f7d2e19dc7; _csrf=Tpb0WeN8LRGzgVLkxEgarI7ZZd46MTYyMTIzMjUxNjI1MTU2MzMwMA' \
-        --data-raw "${jsonRequestdata}" ")
+    bat('curl --location --request POST "http://gitrepsrv:3000/api/v1/repos/aravind.a/GitSync/statuses/444b84683bd56fb3f9755c1d344802c653a9d91f" \
+    --header "Authorization: Bearer feb40616d4d730b6c89a9f74aafe93a3e05230fb" \
+    --header "Content-Type: application/json" \
+        --data-raw "${jsonRequestdata}" ')
 
  
 }
