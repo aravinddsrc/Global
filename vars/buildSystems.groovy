7a338266f844118187b7ce3bc832c7df2d829d28
aravinddsrc <aravind.a@dsrc.co.in>
2021-05-17 22:29:52 +0530

    Update buildSystems.groovy

--- vars/buildSystems.groovy
+++ vars/buildSystems.groovy
@@ -31,7 +31,7 @@ def reportQualityGate(script, Organisation, repository, status, context, descrip
     request.uri ="http://gitrepsrv:3000/api/v1/repos/${Organisation}/${repository}/statuses/${currentSha}"
     request.contentType = 'application/json'
     request.body = jsonRequestdata
-    request.auth.basic'aravind.a', 'Arav123'
+    request.headers= 'Bearer feb40616d4d730b6c89a9f74aafe93a3e05230fb'
 }.post()
     
     
