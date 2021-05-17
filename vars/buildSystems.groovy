92a9e6e821aa3d97010c481732b7b19fa37e0f9c
aravinddsrc <aravind.a@dsrc.co.in>
2021-05-18 00:04:16 +0530

    Update buildSystems.groovy

--- vars/buildSystems.groovy
+++ vars/buildSystems.groovy
@@ -21,9 +21,9 @@ def reportQualityGate(script, Organisation, repository, status, context, descrip
 
     def jsonRequestdata = JsonOutput.toJson(request)
     println  "${jsonRequestdata}"
-    println  "http://gitrepsrv:3000/api/v1/repos/${Organisation}/${repository}/statuses/${currentSha}"
+    def URLValue=  "http://gitrepsrv:3000/api/v1/repos/${Organisation}/${repository}/statuses/${currentSha}"
     
-    def apidata="curl --location --request POST 'http://gitrepsrv:3000/api/v1/repos/aravind.a/GitSync/statuses/444b84683bd56fb3f9755c1d344802c653a9d91f' --header 'Authorization: Bearer feb40616d4d730b6c89a9f74aafe93a3e05230fb' --header 'Content-Type: application/json' --data-raw  ${jsonRequestdata}"
+    def apidata="curl --location --request POST ${URLValue} --header "Authorization: Bearer feb40616d4d730b6c89a9f74aafe93a3e05230fb" --header "Content-Type: application/json" --data-raw  ${jsonRequestdata}"
     
      println  "${apidata}"
     
