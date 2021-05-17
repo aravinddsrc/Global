3bd7a5ab1d2187148a7e673765dd26f03a175df3
aravinddsrc <aravind.a@dsrc.co.in>
2021-05-17 22:38:27 +0530

    Update buildSystems.groovy

--- vars/buildSystems.groovy
+++ vars/buildSystems.groovy
@@ -1,5 +1,4 @@
 @Grab('org.codehaus.groovy.modules.http-builder:http-builder:0.7.1')
-
 import groovyx.net.http.HTTPBuilder
 import static groovyx.net.http.ContentType.*
 import static groovyx.net.http.Method.*
